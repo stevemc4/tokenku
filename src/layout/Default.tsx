bb75556b70f471e224cc659a95ba18d14b871108
Dhika Rizky <deka_r@outlook.com>
2021-06-12 21:55:17 +0700

    Updated default layout

--- src/layout/Default.tsx
+++ src/layout/Default.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import Router from 'next/router'
-import { Box, HStack, IconButton, Icon, theme } from '@chakra-ui/react'
+import { Box, HStack, VStack, IconButton, Icon, theme } from '@chakra-ui/react'
 import { HiOutlineHome, HiOutlineSparkles, HiOutlineQrcode, HiOutlineBell, HiOutlineUser } from 'react-icons/hi'
 import { Capacitor } from '@capacitor/core'
 import { StatusBar, Style } from '@capacitor/status-bar'
@@ -24,11 +24,11 @@ const DefaultLayout = ({ children }: Props): React.ReactElement => {
   }
 
   return (
-    <Box bg="gray.100" minH="100vh">
-      <Box pt="24" px="8" pb="8" w="full">
+    <VStack bg="gray.100" h="100vh">
+      <Box pt="24" px="8" pb="8" w="full" flexGrow={1} overflowY="auto">
         {children}
       </Box>
-      <HStack justifyContent="space-between" bg="white" position="fixed" bottom="0" w="100vw">
+      <HStack justifyContent="space-between" bg="white" w="100vw">
         <IconButton
           onClick={() => handleNavigationButton('/home')}
           aria-label="Home"
@@ -66,7 +66,7 @@ const DefaultLayout = ({ children }: Props): React.ReactElement => {
           p="8" icon={<Icon color="gray.600" w="6" h="6" as={HiOutlineUser} />}
         />
       </HStack>
-    </Box>
+    </VStack>
   )
 }
 
